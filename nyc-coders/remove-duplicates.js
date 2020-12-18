976f549827de156a6a71f7a1e50e2288c2b83d57
osha7 <ogroetz@gmail.com>
2020-12-18 15:45:03 -0700

    remove duplicates

---
+++ nyc-coders/remove-duplicates.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+    let i = 0
+    
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[i] !== nums[j]){
+            i++
+            nums[i] = nums[j]
+        } 
+    }
+    // return arrays length without duplicates
+    return i + 1
+};
