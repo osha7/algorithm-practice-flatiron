5cdbbc6830447a13f0b39639b2e92c92174e0cd4
osha7 <ogroetz@gmail.com>
2021-01-30 10:03:01 -0700

    ztm twitter example and string.length property

--- ztm-algorithms/practice.js
+++ ztm-algorithms/practice.js
@@ -1,3 +1,4 @@
+// Big O = O(n^2) :
 let boxes = [1, 2, 3, 4, 5]
 
 function logPairs(boxes) {
@@ -9,3 +10,36 @@ function logPairs(boxes) {
 }
 
 logPairs(boxes)
+
+// ------------------------------------------------------
+
+// Twitter example
+// find 1st and find nth (last item):
+
+const array = ['hi', 'next', 'last']
+array[0] //oldest tweet    =>   O(1)
+array[array.length - 1] //last tweet (newest) =>  O(1)
+
+// *** //
+
+// compare tweet dates
+const newArray = [{
+    tweet: 'hi',
+    date: 2012
+    }, {
+    tweet: 'next',
+    date: 2014
+    }, {
+    tweet: 'last',
+    date: 2018
+    }];
+
+// comparing each item in the array then comparing dates (nested loops)  => O(n^2)
+
+// *** //
+
+'helsadhgieirhlskbfvnsvsfvn'.length   // Big O depends upon language and how it has built the .length method to work
+// javascript .length has built in method for each string 
+// so for js .length is not a function, it's simply a property
+// .length doesn't work like =>  .length()
+// ^^this above has O(1) bc .length is just a property of the string in js
