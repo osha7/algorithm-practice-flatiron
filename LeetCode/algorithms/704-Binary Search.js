375a2d19d70016276569acd8a6b45e3ec70982a0
osha7 <ogroetz@gmail.com>
2021-02-18 10:58:23 -0700

    88. Merge Sorted Array

---
+++ LeetCode/algorithms/704-Binary Search.js
@@ -0,0 +1,52 @@
+// Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+
+
+// Example 1:
+
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+
+// Example 2:
+
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
+ 
+
+// Note:
+
+// You may assume that all elements in nums are unique.
+// n will be in the range [1, 10000].
+// The value of each element in nums will be in the range [-9999, 9999].
+
+
+// solution:
+
+var search = function(nums, target) {
+    // nums = [-1,0,3,5,9,12], target = 9
+    // Output: 4
+    
+    // Input: nums = [-1,0,3,5,9,12], target = 2
+    // Output: -1
+    
+    let left = 0
+    let right = nums.length - 1
+    
+    while (left <= right) {
+        let med = Math.floor((right + left) / 2)
+        
+        if (nums[med] === target) return med
+        
+        //look at right:
+        if (nums[med] < target) {
+            left = med + 1
+        }
+        
+        //look at left:
+        if (nums[med] > target) {
+            right = med - 1
+        }
+    }
+    return -1
+};
