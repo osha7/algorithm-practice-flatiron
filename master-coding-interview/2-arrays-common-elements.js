086f5defba10efcf1537cb58464982ef8adec5b2
osha7 <ogroetz@gmail.com>
2021-02-26 09:26:30 -0700

    common elements between two arrays

---
+++ master-coding-interview/2-arrays-common-elements.js
@@ -0,0 +1,27 @@
+// if given two arrays:
+
+// does the second array share any common elements to the first array:
+
+
+// solution:
+
+array1 = ['a', 'b', 'c', 'x']
+array2 = ['z', 'y', 'x']
+
+function commonElementsInArrays(array1, array2) {
+    let hash = {}
+
+    for (let i = 0; i < array1.length; i++) {
+        let element = array1[i]
+        hash[element] = true
+    }
+
+    for (let j = 0; j < array2.length; j++) {
+        let newElement = array2[j]
+        if (hash[newElement] === true) {
+            return true
+        }
+    }
+
+    return false
+}
